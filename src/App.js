56fad046da60b0f19ebb4212e426b0bee1a1298c
irishandyesiam <nathan.irish@gmail.com>
2022-09-12 16:59:37 -0500

    Completed useEffect.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
-  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'},{weight: 175, date: '11-23-2021'}])
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'},{weight: 176, date: '11-24-2021'}])
 
   function addNewEntry(entry){
     let tempEntries = [...entries, entry];
@@ -16,6 +17,7 @@ function App() {
     <div>
       <DisplayEntries parentEntries={entries}/>  
       <AddEntryForm addNewEntryProperty={addNewEntry}/>
+      <EntriesChartTracker parentEntries={entries}/>
     </div>
   );
 }
